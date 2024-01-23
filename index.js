781c173617f9b35593500ddf2ce0d47e59577c8b
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-01-23 16:00:59 -0300

    read and write "libros"
    request.http is to verify the correct functioning of the api

--- index.js
+++ index.js
@@ -1,9 +1,58 @@
 import express from "express";
+import fs, { read, writeFile } from "fs";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
+
+const readData = () => {
+    try {
+        const data = fs.readFileSync("./db.json", "utf8");
+        return JSON.parse(data);
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+};
+
+const writeData = (data) => {
+    try {
+        fs.writeFileSync("./db.json", JSON.stringify(data));
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 app.get("/", (req, res) => {
     res.send("Bienvenido a mi API con Node js y Express 😀");
+});
+
+app.get("/libros", (req, res) => {
+    const data = readData();
+    if (data) {
+        res.json(data.libros);
+    } else {
+        res.status(500).send("Error al leer los datos");
+    }
+});
+
+app.get("/libros/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const libro = data.libros.find((libro) => libro.id === id);
+    res.json(libro);
+});
+
+app.post("/libros", (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const nuevoLibro = {
+        id: data.libros.length + 1,
+        ...body,
+    };
+    data.libros.push(nuevoLibro);
+    writeData(data);
+    res.json(nuevoLibro);
 });
 
 app.listen(3000, () => {
